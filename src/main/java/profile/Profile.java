3e8b3a4da8ff4ae7689ae17cb71f6acac4d33a49
John Martin <john.martin@GSOD-JMARTIN.orasi.com>
2015-06-25 17:15:43 -0400

    Updated Profile PageObject Class and AddCoverPhoto test class with objects and methods.

--- src/main/java/profile/Profile.java
+++ src/main/java/profile/Profile.java
@@ -8,12 +8,13 @@ import org.openqa.selenium.support.PageFactory;
 
 /*
  * @Author John Martin
- * @Summary The Profile PageObject Class - contain all relevant objects and methods.
+ * @Summary Profile PageObject Class - contain all relevant objects and methods.
  * @Date 06/24/2015
  * Change Log:
  * <Date>			<Author>		<Change Made>
  * 06/24/2015		John Martin		Added cover photo objects and the initProfile method.
  * 					John Martin		Added the clickSaveButton and btnAddCoverPhoto methods.
+ * 06/25/2015		John Martin		Added the following methods: clickChoosePhotoButton, clickPhotoAlbumsLink, and clickCoverPhotoImage.
  */
 
 public class Profile {
@@ -34,9 +35,11 @@ public class Profile {
 	@FindBy(linkText = "Photo Albums")
 	WebElement lnkPhotoAlbums;
 	
-	//###Need to update this object.
-	@FindBy(className = "scaledImageFitHeight img")
+	@FindBy(xpath = "/html/body/div[7]/div[2]/div/div/div/div[2]/div[2]/div[1]/div/div/table/tbody/tr/td[1]/div/a")
 	WebElement imgPhotoAlbum;
+	
+	@FindBy(xpath = "/html/body/div[7]/div[2]/div/div/div/div[2]/div[2]/div[1]/div/div/table/tbody/tr/td/a/div/img")
+	WebElement imgCoverPhoto;
 	
 	@FindBy(name = "save")
 	WebElement btnSave;
@@ -56,9 +59,29 @@ public class Profile {
 		PageFactory.initElements(driver, this);
 	}
 	
+	//Click the 'Choose From My Photos' button in the dropdown menu.
+	public void clickChoosePhotoButton(){
+		btnChoosePhoto.click();
+	}
+	
+	//Click the 'Photo Albums' link in the pop-up window.
+	public void clickPhotoAlbumsLink(){
+		lnkPhotoAlbums.click();
+	}
+	
 	//Click the 'Add A Cover Photo' button.
 	public void clickAddCoverPhotoButton(){
 		btnAddCoverPhoto.click();
+	}
+	
+	//Click the image for the Photo Album.
+	public void clickPhotoAlbumImage(){
+		imgPhotoAlbum.click();
+	}
+	
+	//Click the image for the Photo Album.
+	public void clickCoverPhotoImage(){
+		imgCoverPhoto.click();
 	}
 	
 	//Click the 'Save' button.
--- src/test/java/smoketests/AddCoverPhoto.java
+++ src/test/java/smoketests/AddCoverPhoto.java
@@ -1,5 +1,25 @@
 package smoketests;
 
+/*
+ * @Author John Martin
+ * @Summary AddCoverPhoto Test Scenario.
+ * @Date 06/24/2015
+ * Change Log:
+ * <Date>			<Author>		<Change Made>
+ * 06/25/2015		John Martin		Outlined the test case scenario shell.
+ */
+
 public class AddCoverPhoto {
+	
+	//Click the 'Update Cover Photo' button.
+	
+	//Click the 'Choose From My Photos' button.
+	
+	//Click the 'Photo Albums' link.
+	
+	//Select the Photo Album.
+	
+	//Select the Photo to be added as a cover photo.
+	
 
 }
